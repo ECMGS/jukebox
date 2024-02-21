364f3b0d3ff921132e62f19878ddb4b29175c27a
jorgerente <jaestefaniah27@gmail.com>
2024-02-21 16:45:23 +0100

    actualizando

--- common/include/fsm_button.h
+++ common/include/fsm_button.h
@@ -55,7 +55,8 @@ typedef struct {
  * @note If you are done with the FSM, you must call fsm_destroy to free memory.
  *
  * @param debounce_time time (in ms) the FSM will wait in intermediate steps to avoid mechanical gltiches.
- *
+ * @param button_id id of the button
+ * 
  * @return fsm_t* pointer to the button FSM.
  */
 fsm_t *fsm_button_new(uint32_t debounce_time, uint32_t button_id);
@@ -64,15 +65,10 @@ fsm_t *fsm_button_new(uint32_t debounce_time, uint32_t button_id);
 /**
  * @brief Initializes all the parameters for an FSM that measures how long the button is pressed.
  *
- * > **TO-DO alumnos:**
- * >
- * > ✅ 1. Cast pointer to button FSM (we already provide this) \n
- * > ✅ 2. Initialize its inner FSM (we already provide this) \n
- * > ✅ 3. Fill all the fields of the button FSM to their initial value \n
- * > ✅ 4. Configure the GPIO of the button accordingly.
- *
  * @param p_fsm pointer to the FSM.
  * @param debounce_time time (in ms) the FSM will wait in intermediate steps to avoid mechanical gltiches.
+ * @param button_id id of the button
+ * 
  */
 void fsm_button_init(fsm_t *p_fsm, uint32_t debounce_time, uint32_t button_id);
 
@@ -85,6 +81,7 @@ void fsm_button_init(fsm_t *p_fsm, uint32_t debounce_time, uint32_t button_id);
  * > ✅ 2. return the value of the duration counter
  *
  * @param p_fsm pointer to the button FSM.
+ * @param button_id id of the button
  * @return uint32_t amount of time (in ms) that the button has been pressed.
  */
 uint32_t fsm_button_get_duration(fsm_t *p_fsm, uint32_t button_id);
@@ -98,6 +95,7 @@ uint32_t fsm_button_get_duration(fsm_t *p_fsm, uint32_t button_id);
  * > ✅ 2. set duration counter to 0
  *
  * @param p_fsm pointer to the button FSM.
+ * @param button_id id of the button
  */
 void fsm_button_reset_duration(fsm_t *p_fsm, uint32_t button_id);
 
