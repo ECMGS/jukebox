de23f5935d4ab47a32aa02760f3ff5edda3e41a8
jorgerente <jaestefaniah27@gmail.com>
2024-03-06 18:42:56 +0100

    terminado fsm_buzzer.c

--- common/src/fsm_buzzer.c
+++ common/src/fsm_buzzer.c
@@ -57,13 +57,88 @@ static bool check_note_end (fsm_t *p_this) {
 
 /* State machine output or action functions */
 
+static void do_melody_start	(fsm_t * p_this)    {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    _start_note(p_this, p_fsm->p_melody->p_notes[0], p_fsm->p_melody->p_durations[0]);
+}
 
+static void do_player_start (fsm_t *p_this) {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    do_melody_start(p_this);
+}
 
+static void do_end_melody (fsm_t *p_this)   {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    port_buzzer_stop(p_fsm->buzzer_id);
+    p_fsm->note_index = 0;
+    p_fsm->user_action = STOP;
+}
 
+static void do_pause (fsm_t *p_this)    {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    port_buzzer_stop(p_fsm->buzzer_id);
+}
 
+static void do_player_stop (fsm_t *p_this)  {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    port_buzzer_stop(p_fsm->buzzer_id);
+    p_fsm->note_index = 0;
+}
+
+static void do_play_note (fsm_t *p_this)    {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    _start_note(p_this, p_fsm->p_melody->p_notes[p_fsm->note_index], p_fsm->p_melody->p_durations[p_fsm->note_index]);
+    p_fsm->note_index++;
+}
+
+static void do_note_end (fsm_t *p_this) {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    port_buzzer_stop(p_fsm->buzzer_id);
+}
+
+static fsm_trans_t fsm_trans_buzzer[] = {
+    {WAIT_START,    check_player_start, WAIT_NOTE,      do_player_start},
+    {WAIT_NOTE,     check_note_end,     PLAY_NOTE,      do_note_end},
+    {PLAY_NOTE,     check_player_stop,  WAIT_START,     do_player_start},
+    {PLAY_NOTE,     check_play_note,    WAIT_NOTE,      do_play_note},
+    {PLAY_NOTE,     check_end_melody,   WAIT_MELODY,    do_end_melody},
+    {PAUSE_NOTE,    check_resume,       PLAY_NOTE,      NULL},
+    {WAIT_MELODY,   check_melody_start, WAIT_NOTE,      do_melody_start},
+    {-1, NULL, -1, NULL}
+};
 
 /* Public functions */
 
+void fsm_buzzer_set_melody (fsm_t *p_this, const melody_t *p_melody)    {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    p_fsm->p_melody = (melody_t *)p_melody;
+}
+
+void fsm_buzzer_set_speed (fsm_t *p_this, double speed) {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    p_fsm->player_speed = speed;
+}
+
+void fsm_buzzer_set_action( fsm_t *p_this, uint8_t action) {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    p_fsm->user_action = action;
+    if (action == STOP) p_fsm->note_index = 0;
+}
+
+uint8_t fsm_buzzer_get_action ( fsm_t *p_this) {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    return p_fsm->user_action;
+}
+
+void fsm_buzzer_init (fsm_t *p_this, uint32_t buzzer_id) {
+    fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
+    p_fsm->buzzer_id = buzzer_id;
+    p_fsm->p_melody = NULL;
+    p_fsm->note_index = 0;
+    p_fsm->user_action = STOP;
+    p_fsm->player_speed = 1.0;
+    port_buzzer_init(p_fsm->buzzer_id);
+}
 
 fsm_t *fsm_buzzer_new(uint32_t buzzer_id)
 {
