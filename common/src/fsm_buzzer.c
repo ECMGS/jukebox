cee17030e6188ceb157a7c768604353699dbb43d
Jorge Alejandro Estefania Hidalgo <jorge.estefania@alumnos.upm.es>
2024-05-14 20:59:11 +0200

    mejorado comando speed

--- common/src/fsm_buzzer.c
+++ common/src/fsm_buzzer.c
@@ -17,124 +17,136 @@
 
 /* State machine input or transition functions */
 
-bool fsm_buzzer_check_activity (fsm_t *p_this)  {
+bool fsm_buzzer_check_activity(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
 
     return p_fsm->user_action == PLAY;
 }
 
-
-static void _start_note(fsm_t *p_this, double freq, uint32_t duration) {
+static void _start_note(fsm_t *p_this, double freq, uint32_t duration)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
 
-    port_buzzer_set_note_duration(p_fsm->buzzer_id, duration * p_fsm->player_speed);
+    port_buzzer_set_note_duration(p_fsm->buzzer_id, duration / p_fsm->player_speed);
     port_buzzer_set_note_frequency(p_fsm->buzzer_id, freq);
 }
 
 /**
  * @brief Check if the melody has started.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_melody_start(fsm_t *p_this)	{
+static bool check_melody_start(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->p_melody != NULL && p_fsm->user_action == PLAY;
 }
 /**
  * @brief Check if the melody has ended.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_end_melody (fsm_t *p_this)    {
+static bool check_end_melody(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->note_index >= p_fsm->p_melody->melody_length;
 }
 /**
  * @brief Check if the player has paused.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_pause	(fsm_t *p_this) {
+static bool check_pause(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->user_action == PAUSE;
-}	
+}
 /**
  * @brief Check if the player has resumed.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_resume(fsm_t *p_this) {
+static bool check_resume(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->user_action == PLAY;
 }
 /**
  * @brief Check if the player has stopped.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_player_stop(fsm_t *p_this) {
+static bool check_player_stop(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->user_action == STOP;
 }
 /**
  * @brief Check if the player has to play a note.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_play_note (fsm_t *p_this) {
+static bool check_play_note(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->user_action == PLAY && p_fsm->note_index < p_fsm->p_melody->melody_length;
 }
 /**
  * @brief Check if the note has ended.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_note_end (fsm_t *p_this) {
+static bool check_note_end(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return port_buzzer_get_note_timeout(p_fsm->buzzer_id);
 }
 /**
  * @brief Check if the player has started.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  * @returns true or false
  */
-static bool check_player_start(fsm_t *p_this)	{
+static bool check_player_start(fsm_t *p_this)
+{
     return check_melody_start(p_this);
 }
 /* State machine output or action functions */
 /**
  * @brief Start a note.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_melody_start	(fsm_t * p_this)    {
+static void do_melody_start(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     _start_note(p_this, p_fsm->p_melody->p_notes[0], p_fsm->p_melody->p_durations[0]);
-    p_fsm->note_index = 1;          // estamos seguros de que es 1 y no 0?
+    p_fsm->note_index = 1; // estamos seguros de que es 1 y no 0?
 }
 /**
  * @brief Start the player.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_player_start (fsm_t *p_this) {
+static void do_player_start(fsm_t *p_this)
+{
     do_melody_start(p_this);
 }
 /**
  * @brief End the melody.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_end_melody (fsm_t *p_this)   {
+static void do_end_melody(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     port_buzzer_stop(p_fsm->buzzer_id);
     p_fsm->note_index = 0;
@@ -142,80 +154,89 @@ static void do_end_melody (fsm_t *p_this)   {
 }
 /**
  * @brief Pause the player.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_pause (fsm_t *p_this)    {
+static void do_pause(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     port_buzzer_stop(p_fsm->buzzer_id);
 }
 /**
  * @brief Stop the player.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_player_stop (fsm_t *p_this)  {
+static void do_player_stop(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     port_buzzer_stop(p_fsm->buzzer_id);
     p_fsm->note_index = 0;
 }
 /**
  * @brief Play a note.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_play_note (fsm_t *p_this)    {
+static void do_play_note(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     _start_note(p_this, p_fsm->p_melody->p_notes[p_fsm->note_index], p_fsm->p_melody->p_durations[p_fsm->note_index]);
     p_fsm->note_index++;
 }
 /**
  * @brief End a note.
- * 
+ *
  * @param p_this Pointer to an fsm_t struct than contains an fsm_buzzer_t.
  */
-static void do_note_end (fsm_t *p_this) {
+static void do_note_end(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     port_buzzer_stop(p_fsm->buzzer_id);
 }
 
 /* State machine transitions */
 static fsm_trans_t fsm_trans_buzzer[] = {
-    {WAIT_START,    check_player_start, WAIT_NOTE,      do_player_start},
-    {WAIT_NOTE,     check_note_end,     PLAY_NOTE,      do_note_end},       
-    {PLAY_NOTE,     check_player_stop,  WAIT_START,     do_player_stop},   
-    {PLAY_NOTE,     check_play_note,    WAIT_NOTE,      do_play_note},      
-    {PLAY_NOTE,     check_end_melody,   WAIT_MELODY,    do_end_melody},     
-    {PLAY_NOTE,     check_pause,        PAUSE_NOTE,     do_pause},
-    {PAUSE_NOTE,    check_resume,       PLAY_NOTE,      NULL},              
-    {WAIT_MELODY,   check_melody_start, WAIT_NOTE,      do_melody_start},   
-    {-1, NULL, -1, NULL}
-};
+    {WAIT_START, check_player_start, WAIT_NOTE, do_player_start},
+    {WAIT_NOTE, check_note_end, PLAY_NOTE, do_note_end},
+    {PLAY_NOTE, check_player_stop, WAIT_START, do_player_stop},
+    {PLAY_NOTE, check_play_note, WAIT_NOTE, do_play_note},
+    {PLAY_NOTE, check_end_melody, WAIT_MELODY, do_end_melody},
+    {PLAY_NOTE, check_pause, PAUSE_NOTE, do_pause},
+    {PAUSE_NOTE, check_resume, PLAY_NOTE, NULL},
+    {WAIT_MELODY, check_melody_start, WAIT_NOTE, do_melody_start},
+    {-1, NULL, -1, NULL}};
 
 /* Public functions */
 
-void fsm_buzzer_set_melody (fsm_t *p_this, const melody_t *p_melody)    {
+void fsm_buzzer_set_melody(fsm_t *p_this, const melody_t *p_melody)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     p_fsm->p_melody = (melody_t *)p_melody;
 }
 
-void fsm_buzzer_set_speed (fsm_t *p_this, double speed) {
+void fsm_buzzer_set_speed(fsm_t *p_this, double speed)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     p_fsm->player_speed = speed;
 }
 
-void fsm_buzzer_set_action( fsm_t *p_this, uint8_t action) {
+void fsm_buzzer_set_action(fsm_t *p_this, uint8_t action)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     p_fsm->user_action = action;
-    if (action == STOP) p_fsm->note_index = 0;
+    if (action == STOP)
+        p_fsm->note_index = 0;
 }
 
-uint8_t fsm_buzzer_get_action ( fsm_t *p_this) {
+uint8_t fsm_buzzer_get_action(fsm_t *p_this)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     return p_fsm->user_action;
 }
 
-void fsm_buzzer_init (fsm_t *p_this, uint32_t buzzer_id) {
+void fsm_buzzer_init(fsm_t *p_this, uint32_t buzzer_id)
+{
     fsm_buzzer_t *p_fsm = (fsm_buzzer_t *)(p_this);
     p_fsm->buzzer_id = buzzer_id;
     p_fsm->p_melody = NULL;
--- common/src/fsm_jukebox.c
+++ common/src/fsm_jukebox.c
@@ -30,19 +30,19 @@
 /* Private functions */
 /**
  * @brief Parse the message received by the USART.
- * 
+ *
  * Given data received by the USART, this function parses the message and extracts the command and the parameter (if available).
- * 
+ *
  * > 1. Split the message by space using function `strtok()` \n
  * > 2. If there's a token (command), copy it to the `p_command` variable. Otherwise, return `false` \n
  * > 3. Extract the parameter (if available). To do so, get the next token using function `strtok()`. If there's a token, copy it to the `p_param` variable. Otherwise, copy an empty string to the `p_param` variable \n
  * > 4. Return `true` indicating that the message has been parsed correctly \n
- * 
+ *
  * @param p_message Pointer to the message received by the USART.
- * @param p_command Pointer to store the command extracted from the message. 
- * @param p_param Pointer to store the parameter extracted from the message. 
- * @return true if the message has been parsed correctly 
- * @return false if the message has not been parsed correctly 
+ * @param p_command Pointer to store the command extracted from the message.
+ * @param p_param Pointer to store the parameter extracted from the message.
+ * @return true if the message has been parsed correctly
+ * @return false if the message has not been parsed correctly
  */
 bool _parse_message(char *p_message, char *p_command, char *p_param)
 {
@@ -74,50 +74,59 @@ bool _parse_message(char *p_message, char *p_command, char *p_param)
     return true;
 }
 
-
-void _set_next_song (fsm_jukebox_t *p_fsm_jukebox) {
+void _set_next_song(fsm_jukebox_t *p_fsm_jukebox)
+{
     fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, STOP);
     p_fsm_jukebox->melody_idx = (p_fsm_jukebox->melody_idx + 1) % MELODIES_MEMORY_SIZE;
-    if (!(p_fsm_jukebox->melodies[p_fsm_jukebox->melody_idx].melody_length > 0)) p_fsm_jukebox->melody_idx = 0;
+    if (!(p_fsm_jukebox->melodies[p_fsm_jukebox->melody_idx].melody_length > 0))
+        p_fsm_jukebox->melody_idx = 0;
     p_fsm_jukebox->p_melody = p_fsm_jukebox->melodies[p_fsm_jukebox->melody_idx].p_name;
     printf(": %s\n", p_fsm_jukebox->p_melody);
     fsm_buzzer_set_melody(p_fsm_jukebox->p_fsm_buzzer, &(p_fsm_jukebox->melodies[p_fsm_jukebox->melody_idx]));
     fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, PLAY);
 }
 
-void _execute_command (fsm_jukebox_t *p_fsm_jukebox, char *p_command, char *p_param) {
-    if (!strcmp(p_command, "play")) {
+void _execute_command(fsm_jukebox_t *p_fsm_jukebox, char *p_command, char *p_param)
+{
+    if (!strcmp(p_command, "play"))
+    {
         fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, PLAY);
-        return ;
+        return;
     }
 
-    if (!strcmp(p_command, "stop")) {
+    if (!strcmp(p_command, "stop"))
+    {
         fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, STOP);
-        return ;
+        return;
     }
 
-    if (!strcmp(p_command, "pause")) {
+    if (!strcmp(p_command, "pause"))
+    {
         fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, PAUSE);
-        return ;
+        return;
     }
 
-    if (!strcmp(p_command, "speed")) {
+    if (!strcmp(p_command, "speed"))
+    {
         double param = atoi(p_param);
-        fsm_buzzer_set_speed(p_fsm_jukebox->p_fsm_buzzer, param);
-        return ;
+        fsm_buzzer_set_speed(p_fsm_jukebox->p_fsm_buzzer, param / 10.0);
+        return;
     }
 
-    if (!strcmp(p_command, "next")) {
+    if (!strcmp(p_command, "next"))
+    {
         _set_next_song(p_fsm_jukebox);
-        return ;
+        return;
     }
-    
-    if (!strcmp(p_command, "select")) {
+
+    if (!strcmp(p_command, "select"))
+    {
         uint32_t melody_selected = atoi(p_param);
 
-        if (p_fsm_jukebox->melodies[melody_selected].melody_length == 0) {
+        if (p_fsm_jukebox->melodies[melody_selected].melody_length == 0)
+        {
             printf("Melody not found\n");
-            return ;
+            return;
         }
 
         fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, STOP);
@@ -128,62 +137,68 @@ void _execute_command (fsm_jukebox_t *p_fsm_jukebox, char *p_command, char *p_pa
         p_fsm_jukebox->p_melody = p_fsm_jukebox->melodies[p_fsm_jukebox->melody_idx].p_name;
 
         fsm_buzzer_set_action(p_fsm_jukebox->p_fsm_buzzer, PLAY);
-        return ;
+        return;
     }
 
-    if(!strcmp(p_command, "info")) {
+    if (!strcmp(p_command, "info"))
+    {
         char msg[USART_OUTPUT_BUFFER_LENGTH];
         sprintf(msg, "Melody: %s\n", p_fsm_jukebox->p_melody);
         fsm_usart_set_out_data(p_fsm_jukebox->p_fsm_usart, msg);
-        return ;
+        return;
     }
 
     fsm_usart_set_out_data(p_fsm_jukebox->p_fsm_usart, "Command not found\n");
 }
 
-
-
 /* State machine input or transition functions */
 
-static bool check_on(fsm_t * p_this) {
+static bool check_on(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     return fsm_button_get_duration(p_fsm->p_fsm_button) > p_fsm->on_off_press_time_ms;
-}	
+}
 
-
-static bool check_off(fsm_t * p_this){
+static bool check_off(fsm_t *p_this)
+{
     return check_on(p_this);
 }
 
-static bool check_melody_finished (fsm_t *p_this) {
+static bool check_melody_finished(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     return fsm_buzzer_get_action(p_fsm->p_fsm_buzzer) == STOP;
 }
 
-static bool check_command_received (fsm_t *p_this){
+static bool check_command_received(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     return fsm_usart_check_data_received(p_fsm->p_fsm_usart);
 }
 
-static bool check_next_song_button (fsm_t *p_this){
+static bool check_next_song_button(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     return fsm_button_get_duration(p_fsm->p_fsm_button) > p_fsm->next_song_press_time_ms;
 }
 
-static bool check_activity (fsm_t *p_this) {
+static bool check_activity(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     return fsm_button_check_activity(p_fsm->p_fsm_button) ||
            fsm_usart_check_activity(p_fsm->p_fsm_usart) ||
            fsm_buzzer_check_activity(p_fsm->p_fsm_buzzer);
 }
 
-static bool check_no_activity (fsm_t *p_this) {
+static bool check_no_activity(fsm_t *p_this)
+{
     return !check_activity(p_this);
 }
 
 /* State machine output or action functions */
 
-static void do_start_up (fsm_t *p_this) {
+static void do_start_up(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     fsm_button_reset_duration(p_fsm->p_fsm_button);
     fsm_usart_enable_rx_interrupt(p_fsm->p_fsm_usart);
@@ -193,13 +208,15 @@ static void do_start_up (fsm_t *p_this) {
     fsm_buzzer_set_action(p_fsm->p_fsm_buzzer, PLAY);
 }
 
-static void do_start_jukebox (fsm_t *p_this) {
+static void do_start_jukebox(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     p_fsm->melody_idx = 1;
     p_fsm->p_melody = p_fsm->melodies[p_fsm->melody_idx].p_name;
 }
 
-static void do_stop_jukebox (fsm_t *p_this) {
+static void do_stop_jukebox(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     fsm_button_reset_duration(p_fsm->p_fsm_button);
     fsm_usart_disable_tx_interrupt(p_fsm->p_fsm_usart);
@@ -208,54 +225,59 @@ static void do_stop_jukebox (fsm_t *p_this) {
     fsm_buzzer_set_action(p_fsm->p_fsm_buzzer, STOP);
 }
 
-static void do_load_next_song (fsm_t *p_this) {
+static void do_load_next_song(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     fsm_button_reset_duration(p_fsm->p_fsm_button);
     _set_next_song(p_fsm);
 }
 
-static void do_read_command (fsm_t *p_this) {
+static void do_read_command(fsm_t *p_this)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     char p_message[USART_INPUT_BUFFER_LENGTH];
     char p_command[USART_INPUT_BUFFER_LENGTH];
     char p_param[USART_INPUT_BUFFER_LENGTH];
     fsm_usart_get_in_data(p_fsm->p_fsm_usart, p_message);
-    if(_parse_message(p_message, p_command, p_param)) _execute_command(p_fsm, p_command, p_param);
+    if (_parse_message(p_message, p_command, p_param))
+        _execute_command(p_fsm, p_command, p_param);
     fsm_usart_reset_input_data(p_fsm->p_fsm_usart);
     p_message[0] = '\0';
 }
 
-static void do_sleep_off (fsm_t *p_this) {
+static void do_sleep_off(fsm_t *p_this)
+{
     port_system_sleep();
 }
 
-static void do_sleep_wait_command(fsm_t * p_this) {
+static void do_sleep_wait_command(fsm_t *p_this)
+{
     port_system_sleep();
-}	
+}
 
-
-static void do_sleep_while_off(fsm_t * p_this) {
+static void do_sleep_while_off(fsm_t *p_this)
+{
     port_system_sleep();
-}	
+}
 
-static void do_sleep_while_on(fsm_t * p_this) {
+static void do_sleep_while_on(fsm_t *p_this)
+{
     port_system_sleep();
-}	
+}
 
 static fsm_trans_t fsm_trans_jukebox[] = {
-    {OFF,               check_on,                START_UP,       do_start_up},
-    {OFF,               check_no_activity,       SLEEP_WHILE_OFF,do_sleep_off},
-    {START_UP,          check_melody_finished,   WAIT_COMMAND,   do_start_jukebox},
-    {WAIT_COMMAND,      check_next_song_button,  WAIT_COMMAND,   do_load_next_song},
-    {WAIT_COMMAND,      check_command_received,  WAIT_COMMAND,   do_read_command},
-    {WAIT_COMMAND,      check_no_activity,       SLEEP_WHILE_ON,do_sleep_wait_command},
-    {WAIT_COMMAND,      check_off,               OFF,            do_stop_jukebox},
-    {SLEEP_WHILE_ON,    check_no_activity,       SLEEP_WHILE_ON, do_sleep_while_on},
-    {SLEEP_WHILE_ON,    check_activity,          WAIT_COMMAND,   NULL},
-    {SLEEP_WHILE_OFF,   check_no_activity,       SLEEP_WHILE_OFF,do_sleep_while_off},
-    {SLEEP_WHILE_OFF,   check_activity,          OFF,   NULL},
-    {-1, NULL, -1, NULL}
-};
+    {OFF, check_on, START_UP, do_start_up},
+    {OFF, check_no_activity, SLEEP_WHILE_OFF, do_sleep_off},
+    {START_UP, check_melody_finished, WAIT_COMMAND, do_start_jukebox},
+    {WAIT_COMMAND, check_next_song_button, WAIT_COMMAND, do_load_next_song},
+    {WAIT_COMMAND, check_command_received, WAIT_COMMAND, do_read_command},
+    {WAIT_COMMAND, check_no_activity, SLEEP_WHILE_ON, do_sleep_wait_command},
+    {WAIT_COMMAND, check_off, OFF, do_stop_jukebox},
+    {SLEEP_WHILE_ON, check_no_activity, SLEEP_WHILE_ON, do_sleep_while_on},
+    {SLEEP_WHILE_ON, check_activity, WAIT_COMMAND, NULL},
+    {SLEEP_WHILE_OFF, check_no_activity, SLEEP_WHILE_OFF, do_sleep_while_off},
+    {SLEEP_WHILE_OFF, check_activity, OFF, NULL},
+    {-1, NULL, -1, NULL}};
 
 /* Public functions */
 fsm_t *fsm_jukebox_new(fsm_t *p_fsm_button, uint32_t on_off_press_time_ms, fsm_t *p_fsm_usart, fsm_t *p_fsm_buzzer, uint32_t next_song_press_time_ms)
@@ -263,11 +285,12 @@ fsm_t *fsm_jukebox_new(fsm_t *p_fsm_button, uint32_t on_off_press_time_ms, fsm_t
     fsm_t *p_fsm = malloc(sizeof(fsm_jukebox_t));
     fsm_init(p_fsm, fsm_trans_jukebox);
     fsm_jukebox_init(p_fsm, p_fsm_button, on_off_press_time_ms, p_fsm_usart, p_fsm_buzzer, next_song_press_time_ms);
-    
+
     return p_fsm;
 }
 
-void fsm_jukebox_init (fsm_t *p_this, fsm_t *p_fsm_button, uint32_t on_off_press_time_ms, fsm_t *p_fsm_usart, fsm_t *p_fsm_buzzer, uint32_t next_song_press_time_ms){
+void fsm_jukebox_init(fsm_t *p_this, fsm_t *p_fsm_button, uint32_t on_off_press_time_ms, fsm_t *p_fsm_usart, fsm_t *p_fsm_buzzer, uint32_t next_song_press_time_ms)
+{
     fsm_jukebox_t *p_fsm = (fsm_jukebox_t *)(p_this);
     fsm_init(p_this, p_this->p_tt);
     p_fsm->p_fsm_button = p_fsm_button;
@@ -286,4 +309,3 @@ void fsm_jukebox_init (fsm_t *p_this, fsm_t *p_fsm_button, uint32_t on_off_press
     p_fsm->melodies[5] = kerosene;
     p_fsm->melodies[6] = kerosene_bass;
 }
-
